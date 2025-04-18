159bbd10a423169f8a91a2b0c1af6103e1414438
김진홍 <jhkim10@miridih.com>
2025-04-19 00:05:01 +0900

    Initial commit

---
+++ src/main/kotlin/flab/kr/catchfood/CatchfoodApplication.kt
@@ -0,0 +1,11 @@
+package flab.kr.catchfood
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatchfoodApplication
+
+fun main(args: Array<String>) {
+	runApplication<CatchfoodApplication>(*args)
+}
---
+++ src/test/kotlin/flab/kr/catchfood/CatchfoodApplicationTests.kt
@@ -0,0 +1,13 @@
+package flab.kr.catchfood
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CatchfoodApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
