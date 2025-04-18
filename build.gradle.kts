6397efdf17ba86a680399168ec00da94ed3136c6
김진홍 <jhkim10@miridih.com>
2025-04-19 00:54:49 +0900

    chore: datasource 메모리디비 h2로 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
