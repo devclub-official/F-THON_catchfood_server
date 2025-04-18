53b3448a03c052478c5a09771dea9d3221c5428c
김진홍 <jhkim10@miridih.com>
2025-04-19 07:35:44 +0900

    fix: 로그 보강

--- src/main/kotlin/flab/kr/catchfood/common/ui/GlobalExceptionHandler.kt
+++ src/main/kotlin/flab/kr/catchfood/common/ui/GlobalExceptionHandler.kt
@@ -2,6 +2,8 @@ package flab.kr.catchfood.common.ui
 
 import flab.kr.catchfood.common.exception.UnauthorizedException
 import flab.kr.catchfood.common.ui.dto.ApiResponse
+import flab.kr.catchfood.config.RequestResponseLoggingFilter
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
@@ -11,6 +13,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(RequestResponseLoggingFilter::class.java)
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -38,6 +44,7 @@ class GlobalExceptionHandler {
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     fun handleAllExceptions(ex: Exception): ApiResponse<Void> {
+        log.error("An unexpected error occurred: ${ex.message}", ex)
         return ApiResponse.fail("An unexpected error occurred: ${ex.message}")
     }
 }
